5cf882e376995eda9e47ee745d3cdec57fb8517c
NischayDesigns4it <nischayv@designi4t.com>
2024-05-29 17:10:36 +0530

    Initial

--- ice/urls.py
+++ ice/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('api/anchor-tag-info/', AnchorTagInfoView.as_view(), name='anchor-tag-info'),
-    path('tag-info/', views.anchor_tag_info, name='tag_info'),
+    path('/', views.anchor_tag_info, name='tag_info'),
 ]
