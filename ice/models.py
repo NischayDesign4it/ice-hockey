9a061f0755ab50ec55f9932876c899d7e7b7ac32
NischayDesigns4it <nischayv@designi4t.com>
2024-06-03 12:52:58 +0530

    Initial

--- ice/models.py
+++ ice/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Transmitter(models.Model):
     transmitterSerialNumber = models.CharField(max_length=20)
     nodeType = models.CharField(max_length=20)
-    allCount = models.IntegerField()
+    allCount = models.IntegerField(null=True, blank=True)
 
 class Read(models.Model):
     transmitter = models.ForeignKey(Transmitter, related_name='reads', on_delete=models.CASCADE)
@@ -16,9 +16,4 @@ class Read(models.Model):
 
 
 
-from django.shortcuts import render
-from .models import Transmitter
 
-def transmitter_list(request):
-    transmitters = Transmitter.objects.all()
-    return render(request, 'your_app/transmitter_list.html', {'transmitters': transmitters})
